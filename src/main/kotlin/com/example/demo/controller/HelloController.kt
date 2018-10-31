647e0267edf62cd1425af7cd981e17736ec44d04
happy2niMac <>
2018-11-01 07:36:11 +0900

    테스트케이스 추가

--- src/main/kotlin/com/example/demo/controller/HelloController.kt
+++ src/main/kotlin/com/example/demo/controller/HelloController.kt
@@ -2,11 +2,13 @@ package com.example.demo.controller
 
 import com.example.demo.service.Hello
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
+@RequestMapping("hello")
 class HelloController {
 
     val counter = AtomicLong()
---
+++ src/test/kotlin/com/example/demo/HelloControllerTest.kt
@@ -0,0 +1,27 @@
+package com.example.demo
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+    
+    @Autowired
+    lateinit var testRestTemplate: TestRestTemplate
+
+    @Test
+    fun rssTest() {
+        val result = testRestTemplate.getForEntity("/hello/hello", String::class.java)
+        Assert.assertNotNull(result)
+        Assert.assertEquals(HttpStatus.OK, result.statusCode)
+		
+		println("$result")
+	}
+}
